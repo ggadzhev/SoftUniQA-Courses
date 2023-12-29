5de0ff0ae8011dc0287b45ab4b3c0aa4208f5ec3
George Gadzhev <gadjev1@abv.bg>
2023-12-29 02:48:05 +0200

    python excercies #3

---
+++ Programming Basics-Python/Conditional Statements - Lab/06. Speed Info.py
@@ -0,0 +1,12 @@
+current_speed = float(input())
+
+if current_speed <= 10:
+    print('slow')
+elif 10 < current_speed <= 50:
+    print('average')
+elif 50 < current_speed <= 150:
+    print('fast')
+elif 150 < current_speed <= 1000:
+    print('ultra fast')
+else:
+    print('extremely fast')
---
+++ Programming Basics-Python/Conditional Statements - Lab/07. Area of Figures.py
@@ -0,0 +1,23 @@
+import math
+
+figure = str(input())
+
+if figure == "square":
+    side_a = float(input())
+    area = side_a * side_a
+    print(f"{area:.3f}")
+elif figure == "rectangle":
+    side_a = float(input())
+    side_b = float(input())
+    area = side_a * side_b
+    print(f"{area:.3f}")
+elif figure == "circle":
+    radius = float(input())
+    area = math.pi * math.pow(radius, 2)
+    print(f"{area:.3f}")
+elif figure == "triangle":
+    side_a = float(input()) 
+    height = float(input())
+    area = side_a * height / 2
+    print(f"{area:.3f}")
+
